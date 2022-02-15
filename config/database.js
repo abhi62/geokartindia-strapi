e4bc8dbdf97e6257fccb027f8e662bcb1b6258e6
abhi62 <abhi.leon.93@gmail.com>
2022-02-15 23:54:17 +0530

    deploy update

--- config/database.js
+++ config/database.js
@@ -1,15 +1,39 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', 'db/data.db'),
+const parse = require("pg-connection-string").parse;
+
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: "default",
+      connection: {
+        client: "postgres",
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    };
+  }
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", "db/data.db"),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  };
+};
