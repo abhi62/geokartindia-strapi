51bef8eba9c5258eb8cced0b3bfb6e8381c66b46
abhi62 <abhi.leon.93@gmail.com>
2022-02-16 00:06:58 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,39 +1,32 @@
 const parse = require("pg-connection-string").parse;
-
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: "default",
-      connection: {
-        client: "postgres",
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false,
-          },
-        },
-      },
-    };
-  }
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "sqlite",
-          filename: env("DATABASE_FILENAME", "db/data.db"),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
-  };
-};
+    debug: false,
+  },
+
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", "db/data.db"),
+      },
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
