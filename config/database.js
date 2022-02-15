4ab6ef053a692fb07477b0a41286d0bd0e542a93
abhi62 <abhi.leon.93@gmail.com>
2022-02-16 00:16:55 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,32 +1,19 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-    debug: false,
-  },
 
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", "db/data.db"),
-      },
-      options: {
-        useNullAsDefault: true,
+module.exports = ({ env }) => {
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", "db/data.db"),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  };
+};
